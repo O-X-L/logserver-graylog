1eccb4ec96d657094f4d5e112d1c97c9f5e66030
Rath Pascal <rath@oxl.at>
2024-12-18 19:11:44 +0100

    add basic monitoring notes

--- monitoring/graylog_journal_size.sh
+++ monitoring/graylog_journal_size.sh
@@ -4,12 +4,12 @@
 
 set -e
 
-JOURNAL_FILE='/usr/share/graylog/data/journal/'
+JOURNAL_DIR='/usr/share/graylog/data/journal/'
 CONFIG_FILE='/etc/graylog/server/graylog.conf'
 DEFAULT_MAX_SIZE_GB=5
 
-size="$(du -s $JOURNAL_FILE | cut -d$'\t' -f1)"
-cnf="$(grep '^message_journal_max_size' < "$CONFIG_FILE" | cut -d '=' -f2 | sed 's|[gG ]||g')"
+size="$(du -s $JOURNAL_DIR | cut -d$'\t' -f1)"
+cnf="$(grep '^message_journal_max_size' < "$CONFIG_FILE" | cut -d '=' -f2 | sed 's|[gGbB ]||g')"
 
 if [ -z "$cnf" ]
 then
